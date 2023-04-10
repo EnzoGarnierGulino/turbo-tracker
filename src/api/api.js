df153523a9b4c3d20b8853310a4bc10d8223615d
Enzo GARNIER-GULINO <enzogarniergulino@gmail.com>
2023-04-10 23:35:45 +0200

    Started to work on getTotalMembers

--- src/api/api.js
+++ src/api/api.js
@@ -1,34 +1,45 @@
 const sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database('../database/TurboTracker.db', (err) => {
+let db = new sqlite3.Database('src/database/TurboTracker.db', (err) => {
     if (err) {
         console.error(err.message);
     }
     else {
         console.log('Connected to the database.');
     }
-
 });
 
+// db.run("CREATE TABLE IF NOT EXISTS logs (total_members INTEGER, total_connected INTEGER, messages_in_channel INTEGER, msg_per_hour INTEGER)");
+// db.run("INSERT INTO logs (total_members, total_connected, messages_in_channel, msg_per_hour) VALUES (?, ?, ?, ?)", [100, 80, 500, 100]);
+// db.run("DELETE FROM logs");
 
-//db.run("CREATE TABLE IF NOT EXISTS logs (total_members INTEGER, total_connected INTEGER, messages_in_channel INTEGER, msg_per_hour INTEGER)");
-//
-// db.run("INSERT INTO test (name) VALUES (?)", "John");
-
-db.all("SELECT * FROM logs", (err, rows) => {
-    if (err) {
-        throw err;
-    }
-    rows.forEach(row => {
-        console.log(row.id, row.name);
-    });
-});
-
-// db.all("SELECT * FROM test", [], { headers: true }, (err, rows) => {
+// db.all("SELECT * FROM logs", (err, rows) => {
 //     if (err) {
 //         throw err;
 //     }
-//     console.table(rows);
+//     rows.forEach(row => {
+//         console.log(row);
+//     });
 // });
 
+function getTotalMembers() {
+    return new Promise((resolve, reject) => {
+        db.all("SELECT total_members FROM logs", (err, rows) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(rows);
+            }
+        });
+    });
+}
 
-db.close();
+getTotalMembers()
+    .then(rows => {
+        rows.forEach(row => {
+            return row.total_members;
+        });
+    })
+    .catch(err => {
+        console.error("Une erreur s'est produite : " + err);
+    });
+
