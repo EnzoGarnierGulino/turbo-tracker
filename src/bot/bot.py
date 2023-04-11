de5f22f5032c890cbf1e2d4c9ce2bece34bb8998
JulesArsac <jules.arsac@alumni.univ-avignon.fr>
2023-04-12 00:12:31 +0200

    Added total members, members connected, number of messages in tracked channel, command to set the tracked channel

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -10,8 +10,18 @@ except ImportError:
     print("Please make sure the functions.py file is in the same directory.")
     exit()
 
+sqliteConnection = sqlite3.connect('../database/TurboTracker.db')
+cur = sqliteConnection.cursor()
+
 global starttime
 starttime = time.time()
+
+global tracked_channel_id
+cur.execute("SELECT tracked_chan FROM storage")
+result = cur.fetchone()
+if result != None :
+    tracked_channel_id = result[0]
+
 
 intents = discord.Intents.all()
 intents.members = True
@@ -27,6 +37,9 @@ async def on_ready():
     await tree.sync(guild=discord.Object(id=1048367362098872360))
     print("Bot connecté en %s secondes" % (time.time() - starttime))
     await client.change_presence(activity=discord.Game(name="Profal Poile"))
+    await getmessagesintrackedchan()
+    gettotalmembers()
+    gettotalconnected()
 
 
 @tree.command(name="quoi", description="Quoi?", guild=discord.Object(id=1048367362098872360))
@@ -34,10 +47,69 @@ async def first_command(interaction):
     await interaction.response.send_message("feur lol")
 
 
-@tree.command(name="getchanid", description="Test to get the channel id", guild=discord.Object(id=1048367362098872360))
-async def first_command(interaction):
+@tree.command(name="track", description="Track the number of messages in this channel (only one per server)", guild=discord.Object(id=1048367362098872360))
+async def chantracking(interaction):
+    global tracked_channel_id
     channel = interaction.channel
-    await interaction.response.send_message(f"The channel ID is {channel.id}")
+    if channel.id == tracked_channel_id:
+        await interaction.response.send_message(f"This channel is already beeing tracked")
+    else:
+        cur.execute("SELECT COUNT(*) FROM storage")
+        count = cur.fetchone()[0]
+        if count == 0:
+            # if table is empty, insert a new row
+            cur.execute("INSERT INTO storage (tracked_chan) VALUES (?)", (channel.id,))
+        else:
+            # if table is not empty, update the existing row
+            cur.execute("UPDATE storage SET tracked_chan = ?", (channel.id,))
+        sqliteConnection.commit()
+        tracked_channel_id = channel.id
+        await interaction.response.send_message(f"The number of messages in this channel is now tracked")
 
+
+async def getmessagesintrackedchan():
+    starttime = time.time()
+    print("Started to count messages in the tracked channel")
+    tracked_channel = client.get_channel(tracked_channel_id)
+    message_count = 0
+    async for message in tracked_channel.history(limit=None):
+        message_count += 1
+    print("Messages counted in %s seconds" % (time.time() - starttime))
+    cur.execute("SELECT COUNT(*) FROM logs")
+    count = cur.fetchone()[0]
+    if count == 0:
+        # if table is empty, insert a new row
+        cur.execute("INSERT INTO logs (messages_in_channel) VALUES (?)", (message_count,))
+    else:
+        # if table is not empty, update the existing row
+        cur.execute("UPDATE logs SET messages_in_channel = ?", (message_count,))
+
+
+def gettotalmembers():
+    guild = client.get_guild(1048367362098872360)
+    cur.execute("SELECT COUNT(*) FROM logs")
+    count = cur.fetchone()[0]
+    if count == 0:
+        # if table is empty, insert a new row
+        cur.execute("INSERT INTO logs (total_members) VALUES (?)", (guild.member_count,))
+    else:
+        # if table is not empty, update the existing row
+        cur.execute("UPDATE logs SET total_members = ?", (guild.member_count,))
+
+
+def gettotalconnected():
+    guild = client.get_guild(1048367362098872360)
+    total_connected = 0
+    for member in guild.members:
+        if (member.status != discord.Status.offline):
+            total_connected += 1
+    cur.execute("SELECT COUNT(*) FROM logs")
+    count = cur.fetchone()[0]
+    if count == 0:
+        # if table is empty, insert a new row
+        cur.execute("INSERT INTO logs (total_connected) VALUES (?)", (total_connected,))
+    else:
+        # if table is not empty, update the existing row
+        cur.execute("UPDATE logs SET total_connected = ?", (total_connected,))
 
 client.run(bot_credentials.gettoken())
