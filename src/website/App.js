7101ccbfc579c415b072b4c0291aeba1a0282661
Enzo GARNIER-GULINO <enzogarniergulino@gmail.com>
2023-04-17 16:08:44 +0200

    Created front-end : cards, labels

--- src/website/App.js
+++ src/website/App.js
@@ -1,9 +1,16 @@
 import './App.css'
 import Title from "./Title.js";
+import Label from "./Label.js";
 
 function App() {
   return (
-      <Title/>
+      <p>
+        <Title/>
+        <Label text={"Nombre d'utilisateurs : "}/>
+        <Label text={"Nombre d'utilisateurs connectés :"}/>
+          <Label text={"Nombre de messages dans général : "}/>
+          <Label text={"Nombre de messages par heure :"}/>
+      </p>
   );
 }
 
---
+++ src/website/Label.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Label = ({ text }) => {
+    return <h2 className="label">{text}</h2>;
+};
+
+export default Label;
